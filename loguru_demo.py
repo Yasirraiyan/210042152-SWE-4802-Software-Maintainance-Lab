75743b23a23174f9ae91a0759f2c4234d551f6b5
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-06-12 11:41:30 +0600

    Delete loguru_demo.py

--- loguru_demo.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-logger.trace("A trace message")
-logger.debug("A debug message")
-logger.info("An info message")
-logger.warning("A warning message")
-logger.error("An error message")
-logger.critical("A critical message")
